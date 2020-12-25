a1d2e243daaf636cdccae9357e6b94ead8869385
NoJamesNo <jamesnoh123@gmail.com>
2020-12-24 20:25:04 -0500

    Added footer. (#2)
    * Added footer.
    
    * Added footer.
    
    * made changes

---
+++ src/containers/Body/Body.styles.js
@@ -0,0 +1,9 @@
+const styles = {
+   body: {
+      display: 'flex',
+      flex: '1',
+      flexGrow: '2',
+   }
+}
+
+export default styles;
---
+++ src/containers/Footer/Footer.styles.js
@@ -0,0 +1,32 @@
+import { findByLabelText } from "@testing-library/react";
+
+const FOOTER_PADDING = 10;
+const FOOTER_MARGIN = 50;
+
+const styles = {
+   footer: {
+      display:'flex',
+      height: 50,
+<<<<<<< HEAD
+=======
+      color: 'black',
+>>>>>>> 5517d9bd07abb42124183f29e372a8a3a2b74c37
+      fontSize: 30,
+      paddingLeft: FOOTER_PADDING,
+      paddingRight: FOOTER_PADDING,
+      paddingTop: FOOTER_PADDING,
+      paddingBottom: FOOTER_PADDING,
+      backgroundColor: '#c4c4c4',
+      alignItems: 'center',
+<<<<<<< HEAD
+      justifyContent:'center',   
+   }
+=======
+      justifyContent:'center',
+      
+   }
+
+>>>>>>> 5517d9bd07abb42124183f29e372a8a3a2b74c37
+}
+
+export default styles;
