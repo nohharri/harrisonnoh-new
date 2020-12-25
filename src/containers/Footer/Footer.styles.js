14d2bc6a58c67a43e8f84ab6a6ec4f150ad96ec0
Harrison Noh <nohharri@github.com>
2020-12-24 20:37:42 -0500

    fixed syntax issues

--- src/containers/Footer/Footer.styles.js
+++ src/containers/Footer/Footer.styles.js
@@ -1,16 +1,11 @@
 import { findByLabelText } from "@testing-library/react";
 
 const FOOTER_PADDING = 10;
-const FOOTER_MARGIN = 50;
 
 const styles = {
    footer: {
       display:'flex',
       height: 50,
-<<<<<<< HEAD
-=======
-      color: 'black',
->>>>>>> 5517d9bd07abb42124183f29e372a8a3a2b74c37
       fontSize: 30,
       paddingLeft: FOOTER_PADDING,
       paddingRight: FOOTER_PADDING,
@@ -18,15 +13,8 @@ const styles = {
       paddingBottom: FOOTER_PADDING,
       backgroundColor: '#c4c4c4',
       alignItems: 'center',
-<<<<<<< HEAD
-      justifyContent:'center',   
+      justifyContent:'center',         
    }
-=======
-      justifyContent:'center',
-      
-   }
-
->>>>>>> 5517d9bd07abb42124183f29e372a8a3a2b74c37
 }
 
 export default styles;
