4bd7159c2dd4d578a2c5d4d279cbaec289b2ac16
Jamesnoh <jamesnoh123@gmail.com>
2020-12-30 14:37:57 -0500

    Added favicon and logo

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,12 +1,14 @@
 import React from 'react';
-
 import styles from './Header.module.scss';
+import logo from '../../static/kolor.png';
 
 export default class Header extends React.Component {
     render() {
         return (
             <div className={styles.container}>
-                <div className={styles.logo}>Harrison</div>
+                <div className={styles.logo}>
+                    <img alt="profile" className={styles.logoImg} src={logo} />
+Harrison</div>
                 <div className={styles.navigation}>
                     <span className={styles.navigationLink}>design</span>
                     <span className={styles.navigationLink}>photography</span>
