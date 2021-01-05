08b6f8a58234173bb88d6b8e4e43d31ec07a7ae8
Harrison Noh <nohharri@github.com>
2021-01-05 16:11:22 -0500

    Added responsive navigation buttons

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,6 +1,9 @@
 import React from 'react';
+import { faPaintBrush, faCameraRetro } from '@fortawesome/free-solid-svg-icons';
+
 import styles from './Header.module.scss';
 import logo from '../../static/kolor.png';
+import NavigationLink from '../../components/NavigationLink/NavigationLink';
 
 export default class Header extends React.Component {
     render() {
@@ -8,10 +11,10 @@ export default class Header extends React.Component {
             <div className={styles.container}>
                 <div className={styles.logo}>
                     <img alt="profile" className={styles.logoImg} src={logo} />
-Harrison</div>
+                    Harrison</div>
                 <div className={styles.navigation}>
-                    <span className={styles.navigationLink}>design</span>
-                    <span className={styles.navigationLink}>photography</span>
+                    <NavigationLink icon={faPaintBrush} className={styles.navigationLink}>design</NavigationLink>
+                    <NavigationLink icon={faCameraRetro} className={styles.navigationLink}>photography</NavigationLink>
                 </div>
             </div>
         );
